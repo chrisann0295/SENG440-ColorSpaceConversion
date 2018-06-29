8ba3c751d284509f6a9a22dee0526fabdd813dad
Genevieve Luyt <gluyt@localhost.localdomain>
2018-06-28 21:29:22 -0700

    Add upsampling in ycc to rgb conversion

--- matlab/rgb2ycc.m
+++ matlab/rgb2ycc.m
@@ -1,7 +1,7 @@
 FILENAME = 'image.jpg';
-OUTPUT_Y = 'y.txt';
-OUTPUT_CB = 'cb.txt';
-OUTPUT_CR = 'cr.txt';
+OUTPUT_Y = 'y_expected.txt';
+OUTPUT_CB = 'cb_expected.txt';
+OUTPUT_CR = 'cr_expected.txt';
 
 RGB = imread(FILENAME);
 
@@ -10,6 +10,10 @@ YCBCR = rgb2ycbcr(RGB);
 Y = YCBCR(:,:,1);
 Cb = YCBCR(:,:,2);
 Cr = YCBCR(:,:,3);
+
+% Downsample
+Cb = Cb(1:2:end,1:2:end);
+Cr = Cr(1:2:end,1:2:end);
 
 dlmwrite(OUTPUT_Y, Y, ' ');
 dlmwrite(OUTPUT_CB, Cb, ' ');
--- matlab/ycc2rgb.m
+++ matlab/ycc2rgb.m
@@ -3,13 +3,19 @@ FILENAME_Y = 'y.txt';
 FILENAME_CB = 'cb.txt';
 FILENAME_CR = 'cr.txt';
 
+% see https://www.mathworks.com/help/images/ref/imresize.html#buxswkh-1-method
+% choose from 'nearest' or 'bilinear'
+UPSAMPLE_METHOD = 'nearest';
+
 % Load 2D arrays from file
 % Values should be space separated
 Y = uint8(importdata(FILENAME_Y));
 Cb = uint8(importdata(FILENAME_CB));
 Cr = uint8(importdata(FILENAME_CR));
 
-% TODO: Upsample
+% Upsample
+Cb = imresize(Cb, size(Y), UPSAMPLE_METHOD);
+Cr = imresize(Cr, size(Y), UPSAMPLE_METHOD);
 
 % Combine the arrays into a 3D array
 % The size will be YCBCR[height][width][3]
